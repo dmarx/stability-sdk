01c3b46e489bee98f7260c4f10cfa4645c723897
David Marx <david.marx84@gmail.com>
2022-10-14 12:02:37 -0700

    Update test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,9 @@
+from stability_sdk import client
+
 def test_client_import():
     from stability_sdk import client
+    assert True
+
+def test_StabilityInference_init:
+    class_instance = client.StabilityInference()
     assert True
