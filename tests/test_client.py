a82a1338d9587e537a18862456675790b7c36819
David Marx <david.marx84@gmail.com>
2022-10-14 19:51:44 -0700

    Update test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,9 @@
 import pytest
+from PIL import Image
 
 from stability_sdk import client
-
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import stability_sdk.interfaces.gooseai.generation.generation_pb2_grpc as generation_grpc
 
 def test_client_import():
     from stability_sdk import client
@@ -43,3 +45,26 @@ def test_truncate_fit():
         idx=0, 
         max=99)
     assert True
+
+def test_image_to_prompt()
+    im = Image.new('RGB',(1,1))
+    prompt = image_to_prompt(im, init=False, mask=False)
+    assert isinstance(prompt, generation.Prompt)
+
+def test_image_to_prompt_init()
+    im = Image.new('RGB',(1,1))
+    prompt = image_to_prompt(im, init=True, mask=False)
+    assert isinstance(prompt, generation.Prompt)
+
+def test_image_to_prompt_mask()
+    im = Image.new('RGB',(1,1))
+    prompt = image_to_prompt(im, init=False, mask=True)
+    assert isinstance(prompt, generation.Prompt)
+
+def test_image_to_prompt_init_mask()
+    im = Image.new('RGB',(1,1))
+    try:
+        prompt = image_to_prompt(im, init=True, mask=True)
+        assert False
+    except ValueError:
+        assert True
